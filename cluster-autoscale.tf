21628c0e348c8c9c59ff4310e3d2027f602268e1
Hossein Vahedi <mcs.hsn.vahedi@gmail.com>
2021-03-17 23:00:56 +0330

    d

--- cluster-autoscale.tf
+++ cluster-autoscale.tf
@@ -58,7 +58,9 @@ data "aws_iam_policy_document" "cluster_autoscaler" {
 
 provider "helm" {
   kubernetes {
-    config_path = "~/.kube/config"
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
   }
 }
 
