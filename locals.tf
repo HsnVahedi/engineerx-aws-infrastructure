47275560c86331f0164d3fb15e880165bbc25436
Hossein Vahedi <mcs.hsn.vahedi@gmail.com>
2021-03-18 04:41:23 +0330

    d

--- locals.tf
+++ locals.tf
@@ -1,4 +1,37 @@
 locals {
   k8s_service_account_namespace = "kube-system"
   k8s_service_account_name      = "cluster-autoscaler-aws-cluster-autoscaler-chart"
+
+  # non-default context name to protect from using wrong kubeconfig
+  kubeconfig_context = "_terraform-kustomization-${var.cluster_name}_"
+
+  kubeconfig = {
+    apiVersion = "v1"
+    clusters = [
+      {
+        name = local.kubeconfig_context
+        cluster = {
+          certificate-authority-data = data.aws_eks_cluster.cluster.certificate_authority.0.data
+          server                     = data.aws_eks_cluster.cluster.endpoint
+        }
+      }
+    ]
+    users = [
+      {
+        name = local.kubeconfig_context
+        user = {
+          token = data.aws_eks_cluster_auth.cluster.token
+        }
+      }
+    ]
+    contexts = [
+      {
+        name = local.kubeconfig_context
+        context = {
+          cluster = local.kubeconfig_context
+          user    = local.kubeconfig_context
+        }
+      }
+    ]
+  }
 }
--- providers.tf
+++ providers.tf
@@ -28,41 +28,6 @@ provider "kubernetes" {
   token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
-locals {
-  # non-default context name to protect from using wrong kubeconfig
-  kubeconfig_context = "_terraform-kustomization-${var.cluster_name}_"
-
-  kubeconfig = {
-    apiVersion = "v1"
-    clusters = [
-      {
-        name = local.kubeconfig_context
-        cluster = {
-          certificate-authority-data = data.aws_eks_cluster.cluster.certificate_authority.0.data
-          server                     = data.aws_eks_cluster.cluster.endpoint
-        }
-      }
-    ]
-    users = [
-      {
-        name = local.kubeconfig_context
-        user = {
-          token = data.aws_eks_cluster_auth.cluster.token
-        }
-      }
-    ]
-    contexts = [
-      {
-        name = local.kubeconfig_context
-        context = {
-          cluster = local.kubeconfig_context
-          user    = local.kubeconfig_context
-        }
-      }
-    ]
-  }
-}
-
 provider "kustomization" {
   kubeconfig_raw = yamlencode(local.kubeconfig)
   context        = local.kubeconfig_context
