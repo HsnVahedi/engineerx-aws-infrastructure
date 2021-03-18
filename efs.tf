ad7018b9aba1f11641492e5a17e13936dfeedd0b
Hossein Vahedi <mcs.hsn.vahedi@gmail.com>
2021-03-18 11:53:33 +0330

    d

--- efs.tf
+++ efs.tf
@@ -21,10 +21,20 @@ resource "aws_efs_mount_target" "media_efs_mount_targets" {
   security_groups = [aws_security_group.eks_efs_group.id]
 }
 
-data "kustomization" "kustomization_info" {
-  provider = kustomization
-  path = "github.com/kubernetes-sigs/aws-efs-csi-driver/deploy/kubernetes/overlays/stable/?ref=release-1.0"
+
+
+resource "helm_release" "cluster_autoscaler" {
+  name       = "aws-efs-csi-driver"
+
+  repository = "https://kubernetes-sigs.github.io/aws-efs-csi-driver/"
+  chart      = "aws-efs-csi-driver/aws-efs-csi-driver"
+  namespace  = "kube-system"
 }
+
+# data "kustomization" "kustomization_info" {
+#   provider = kustomization
+#   path = "github.com/kubernetes-sigs/aws-efs-csi-driver/deploy/kubernetes/overlays/stable/?ref=release-1.0"
+# }
 
 # resource "kustomization_resource" "kustomization_resources" {
 #   provider = kustomization
@@ -34,60 +44,59 @@ data "kustomization" "kustomization_info" {
 #   manifest = data.kustomization.kustomization_info.manifests[each.value]
 # }
 
-resource "kubernetes_storage_class" "efs_sc" {
-  metadata {
-    name = "efs-sc"
-  }
-  storage_provisioner = "efs.csi.aws.com"
-}
 
-resource "kubernetes_persistent_volume" "media_efs_pvc" {
-  metadata {
-    name = "media-efs-pvc"
-  }
 
-  spec {
-    capacity = {
-      storage = "5Gi"
-    }
 
-    access_modes                     = ["ReadWriteMany"]
-    persistent_volume_reclaim_policy = "Retain"
-    storage_class_name               = "efs-sc"
-    persistent_volume_source {
-      csi {
-        driver = "efs.csi.aws.com"
-        volume_handle =  aws_efs_file_system.media_efs.id
-      }
-    }
+
+
+
+
+# resource "kubernetes_storage_class" "efs_sc" {
+#   metadata {
+#     name = "efs-sc"
+#   }
+#   storage_provisioner = "efs.csi.aws.com"
+# }
+
+# resource "kubernetes_persistent_volume" "media_efs_pvc" {
+#   metadata {
+#     name = "media-efs-pvc"
+#   }
+
+#   spec {
+#     capacity = {
+#       storage = "5Gi"
+#     }
+
+#     access_modes                     = ["ReadWriteMany"]
+#     persistent_volume_reclaim_policy = "Retain"
+#     storage_class_name               = "efs-sc"
+#     persistent_volume_source {
+#       csi {
+#         driver = "efs.csi.aws.com"
+#         volume_handle =  aws_efs_file_system.media_efs.id
+#       }
+#     }
     
-  }
+#   }
 
-}
+# }
 
-resource "kubernetes_persistent_volume_claim" "efs_storage_claim" {
-  metadata {
-    name      = "efs-storage-claim"
-    namespace = "storage"
-  }
+# resource "kubernetes_persistent_volume_claim" "efs_storage_claim" {
+#   metadata {
+#     name      = "efs-storage-claim"
+#     namespace = "storage"
+#   }
 
-  spec {
-    access_modes = ["ReadWriteMany"]
+#   spec {
+#     access_modes = ["ReadWriteMany"]
 
-    resources {
-      requests = {
-        storage = "5Gi"
-      }
-    }
+#     resources {
+#       requests = {
+#         storage = "5Gi"
+#       }
+#     }
 
-    storage_class_name = "efs-sc"
-  }
-}
-
-resource "helm_release" "cluster_autoscaler" {
-  name       = "aws-efs-csi-driver"
-
-  repository = "https://kubernetes-sigs.github.io/aws-efs-csi-driver/"
-  chart      = "aws-efs-csi-driver/aws-efs-csi-driver"
-  namespace  = "kube-system"
-}
+#     storage_class_name = "efs-sc"
+#   }
+# }
