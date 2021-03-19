13b19f3bdb481c48e78b82e68ce936ebb126e108
Hossein Vahedi <mcs.hsn.vahedi@gmail.com>
2021-03-19 20:58:54 +0330

    d

--- efs.tf
+++ efs.tf
@@ -16,7 +16,7 @@ resource "aws_efs_file_system" "media_efs" {
 
 resource "aws_efs_mount_target" "media_efs_mount_targets" {
   count = 3
-  subnet_id      = module.vpc.private_subnets[count.index] 
+  subnet_id      = module.vpc.public_subnets[count.index] 
   file_system_id = aws_efs_file_system.media_efs.id
   security_groups = [aws_security_group.eks_efs_group.id]
 }
@@ -27,7 +27,7 @@ resource "aws_efs_file_system" "static_efs" {
 
 resource "aws_efs_mount_target" "static_efs_mount_targets" {
   count = 3
-  subnet_id      = module.vpc.private_subnets[count.index] 
+  subnet_id      = module.vpc.public_subnets[count.index] 
   file_system_id = aws_efs_file_system.static_efs.id
   security_groups = [aws_security_group.eks_efs_group.id]
 }
--- vpc.tf
+++ vpc.tf
@@ -10,13 +10,13 @@ module "vpc" {
   public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
   enable_dns_hostnames = true
 
-  public_subnet_tags = {
+  public_subnet_tags   = {
     "kubernetes.io/cluster/${var.cluster_name}" = "shared"
-    "kubernetes.io/role/elb"                      = "1"
+    "kubernetes.io/role/elb"                    = "1"
   }
 
-  private_subnet_tags = {
-    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
-    "kubernetes.io/role/internal-elb"                      = "1"
-  }
+  # private_subnet_tags  = {
+  #   "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+  #   "kubernetes.io/role/internal-elb"           = "1"
+  # }
 }
