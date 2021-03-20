162b8374a17f900a860848f1ae717367b25ce75b
Hossein Vahedi <mcs.hsn.vahedi@gmail.com>
2021-03-21 00:32:08 +0330

    d

--- rds.tf
+++ rds.tf
@@ -38,6 +38,7 @@ module "db" {
   identifier              = "engineerx"
   instance_class          = "db.t3.micro"
   engine                  = "postgres"
+  family                = "postgres11"
   subnet_ids              = module.vpc.public_subnets
   vpc_security_group_ids  = [aws_security_group.rds_sg.id]
   username                = "engineerx"
