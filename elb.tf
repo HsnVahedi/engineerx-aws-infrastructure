bc898d030a9e851d89f08b910c65ad1e7a43c7d5
Hossein Vahedi <mcs.hsn.vahedi@gmail.com>
2021-03-22 04:32:23 +0430

    d

--- elb.tf
+++ elb.tf
@@ -21,3 +21,11 @@ resource "kubernetes_service" "ingress" {
     type = "LoadBalancer"
   }
 }
+
+locals {
+  lb_name = split("-", split(".", kubernetes_service.example.status.0.load_balancer.0.ingress.0.hostname).0).0
+}
+
+output "load_balancer_name" {
+  value = local.lb_name
+}
